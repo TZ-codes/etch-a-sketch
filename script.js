c81a2ec7df6fe90daf5600d5072b6296a0571645
Tyler <Faulknert2016@gmail.com>
2022-09-07 21:15:16 -0400

    added event listener that changes background color of divs

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const container = document.querySelector('.container');
 
+
+
 const grid = (num) => {
   for (i=0; i<num; i++) {
     const row = container.appendChild(document.createElement("div"));
+    row.classList.add('row')
+    console.log(row);
     for(j=0; j<num; j++) {
       const column = document.createElement("div");
-      column.classList.add("grid");
+      column.classList.add("column");
+      column.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+        });
       row.appendChild(column);
+      console.log(column);
+
     }
   }
 }
 
-grid(2);
+grid(20);
+
+
+
+// let boxes = document.querySelectorAll('.column');
+// boxes.forEach(box => {
+//   box.addEventListener('mouseover', (e) => {
+//     e.target.style.backgroundColor = 'black';
+//   })
+// })
+
